8e095b74c5a604b952f57e06c66757bd8bf29408
arteys <calamatica@gmail.com>
2023-01-06 22:14:01 +0500

    Data now return in usable form

--- CP_AN_table_processing.py
+++ CP_AN_table_processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def number_from_imagename(image_name):
+def number_type_from_imagename(image_name):
     i = 0
     for symbol in image_name:
         if symbol.isdigit():
@@ -8,8 +8,27 @@ def number_from_imagename(image_name):
         if symbol.isalpha():
             break
     image_number = int(image_name[0:i])
-    print(i)
-    return image_number
+    image_type = image_name[i:12+i]
+
+    return image_number,image_type
+
+def mean_and_stdev_by_type(dataframe,image_type):
+    current_df = dataframe.loc[dataframe["Image type"] == image_type]
+    cells = current_df["N Cells"].mean()
+    cells_stdev = current_df["N Cells"].std()
+    apoptotic = current_df["N Apoptotic"].mean()
+    apoptotic_stdev = current_df["N Apoptotic"].std()
+    necrotic = current_df["N Necrotic"].mean()
+    necrotic_stdev = current_df["N Necrotic"].std()
+    na = current_df["N NA"].mean()
+    na_stdev = current_df["N NA"].std()
+
+    data = pd.DataFrame({'Cells':[cells], 'Apoptotic':[apoptotic],'Necrotic':[necrotic], 'NA':[na], 'Cells std':[cells_stdev], 
+        'Apoptotic std':[apoptotic_stdev], 'Necrotic std':[necrotic_stdev], 'NA std':[na_stdev]})
+
+    data_round = data.round(0)
+
+    return data_round
 
 # cells_path = "./PhD_scripts/Data examples/CP_AN_table_processing/Cells.csv"
 cells_path = "C:/Users/Modern/Desktop/Python/PhD-scripts/Data examples/CP_AN_table_processing/Cells.csv"
@@ -26,7 +45,7 @@ an_df = pd.read_csv(an_path)
 
 image_names = pd.unique(cells_df.loc[:,"FileName_Original"]) #Extract unique names of images
 
-result_df = pd.DataFrame({'Image number':[], 'Image name':[], 'N Cells':[], 'N Apoptotic':[], 'N Necrotic':[], 'N NA':[]})
+result_df = pd.DataFrame({'Image number':[], 'Image name':[], 'Image type':[], 'N Cells':[], 'N Apoptotic':[], 'N Necrotic':[], 'N NA':[]})
 
 for name in image_names:
     cells_df_current = cells_df.loc[cells_df["FileName_Original"] == name] #Extract all rows with same image name
@@ -40,20 +59,21 @@ for name in image_names:
     an_df_number = an_df_current.shape[0]
 
     #Extract original image number from image name
-    image_number = number_from_imagename(name)
-    print(name)
+    image_number,image_type = number_type_from_imagename(name)
 
     #Create df with current data 
-    result_current =  pd.DataFrame({'Image number':[image_number], 'Image name':[name], 'N Cells':[cells_number], 'N Apoptotic':[apoptosis_number], 
+    result_current =  pd.DataFrame({'Image number':[image_number], 'Image name':[name],'Image type':[image_type], 'N Cells':[cells_number], 'N Apoptotic':[apoptosis_number], 
         'N Necrotic':[necrosis_number], 'N NA':[an_df_number]})
 
     #Append current data to result df
-    result_df = result_df.append(result_current)
+    # result_df = result_df.append(result_current)
+    result_df = pd.concat([result_current, result_df])
 
 result_df = result_df.sort_values(by=['Image name'])
 result_sorted_df = result_df.sort_values(by=['Image number'])
 
-print(result_sorted_df)
+data = mean_and_stdev_by_type(result_sorted_df,'ApNec 300-20')
+print(data)
 
 
 
