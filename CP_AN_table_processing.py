09c85108d7d83dbb64913dea1dc2aae2bf89e524
arteys <calamatica@gmail.com>
2023-01-07 01:36:53 +0500

    Now the script generates a graph. Also add thumbnails to readme

--- CP_AN_table_processing.py
+++ CP_AN_table_processing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def number_type_from_imagename(image_name):
     i = 0
@@ -23,12 +25,15 @@ def mean_and_stdev_by_type(dataframe,image_type):
     na = current_df["N NA"].mean()
     na_stdev = current_df["N NA"].std()
 
+    necrotic = necrotic - na #Correction on multiple counting
+
     data = pd.DataFrame({'Cells':[cells], 'Apoptotic':[apoptotic],'Necrotic':[necrotic], 'NA':[na], 'Cells std':[cells_stdev], 
         'Apoptotic std':[apoptotic_stdev], 'Necrotic std':[necrotic_stdev], 'NA std':[na_stdev]})
 
     data_round = data.round(0)
 
     return data_round
+
 
 # cells_path = "./PhD_scripts/Data examples/CP_AN_table_processing/Cells.csv"
 cells_path = "C:/Users/Modern/Desktop/Python/PhD-scripts/Data examples/CP_AN_table_processing/Cells.csv"
@@ -46,6 +51,7 @@ an_df = pd.read_csv(an_path)
 image_names = pd.unique(cells_df.loc[:,"FileName_Original"]) #Extract unique names of images
 
 result_df = pd.DataFrame({'Image number':[], 'Image name':[], 'Image type':[], 'N Cells':[], 'N Apoptotic':[], 'N Necrotic':[], 'N NA':[]})
+result_df_sns = pd.DataFrame({'Image type':[], 'Cells type':[], 'Cells number':[]})
 
 for name in image_names:
     cells_df_current = cells_df.loc[cells_df["FileName_Original"] == name] #Extract all rows with same image name
@@ -56,24 +62,29 @@ for name in image_names:
     cells_number = cells_df_current.shape[0] #Count number of rows with same image name
     apoptosis_number = apoptosis_df_current.shape[0]
     necrosis_number = necrosis_df_current.shape[0]
-    an_df_number = an_df_current.shape[0]
+    an_number = an_df_current.shape[0]
 
     #Extract original image number from image name
     image_number,image_type = number_type_from_imagename(name)
 
     #Create df with current data 
     result_current =  pd.DataFrame({'Image number':[image_number], 'Image name':[name],'Image type':[image_type], 'N Cells':[cells_number], 'N Apoptotic':[apoptosis_number], 
-        'N Necrotic':[necrosis_number], 'N NA':[an_df_number]})
+        'N Necrotic':[necrosis_number], 'N NA':[an_number]})
+
+    current_df_sns = pd.DataFrame({'Image type':[image_type,image_type,image_type,image_type], 'Type':["Cells","Apoptotic","Necrotic","Late Apoptotic"],
+    'Cells number':[cells_number,apoptosis_number,necrosis_number,an_number]})
 
     #Append current data to result df
     # result_df = result_df.append(result_current)
     result_df = pd.concat([result_current, result_df])
+    result_df_sns = pd.concat([current_df_sns,result_df_sns])
 
-result_df = result_df.sort_values(by=['Image name'])
-result_sorted_df = result_df.sort_values(by=['Image number'])
+# result_df = result_df.sort_values(by=['Image name'])
+# result_sorted_df = result_df.sort_values(by=['Image number'])
 
-data = mean_and_stdev_by_type(result_sorted_df,'ApNec 300-20')
-print(data)
+# data = mean_and_stdev_by_type(result_sorted_df,'ApNec 300-20')
+print(result_df_sns)
 
 
-
+sns.barplot(data=result_df_sns, x="Image type", y="Cells number", hue = 'Type', errorbar = 'sd')
+plt.show()
--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 
 Circles correspond to expected equvalent diamteres (dotted line), mean (solid line) and sd. dev (semitransparent line) of objects.
 
-![Circle graph](https://github.com/arteys/PhD-scripts/blob/main/Circle%20graph.png?raw=true)
+![Circle graph](./Thumbnails/Circle%20graph.png)
 
 # AN vero + staridst_masks.cpproj
 
 Cell profiler pipeline
+
+![Cell profiler pipeline](./Thumbnails/Cells%20AN%20after%20CP.png)
+
+# CP_AN_table_processing.py
+
+Script for postprocessing CellProfiler data and drawing graphs
+
+![Cell profiler pipeline](./Thumbnails/Cells_counted_seaborn.png)
