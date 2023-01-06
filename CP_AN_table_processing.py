71caac1a618c874b5fa7c6ab99594aaa748e8845
arteys <calamatica@gmail.com>
2023-01-06 21:10:50 +0500

    Now with sorting

--- CP_AN_table_processing.py
+++ CP_AN_table_processing.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
+def number_from_imagename(image_name):
+    i = 0
+    for symbol in image_name:
+        if symbol.isdigit():
+            i = i+1
+        if symbol.isalpha():
+            break
+    image_number = int(image_name[0:i])
+    print(i)
+    return image_number
+
 # cells_path = "./PhD_scripts/Data examples/CP_AN_table_processing/Cells.csv"
 cells_path = "C:/Users/Modern/Desktop/Python/PhD-scripts/Data examples/CP_AN_table_processing/Cells.csv"
 apoptosis_path = "C:/Users/Modern/Desktop/Python/PhD-scripts/Data examples/CP_AN_table_processing/Apoptotic_Cells.csv"
@@ -15,6 +26,8 @@ an_df = pd.read_csv(an_path)
 
 image_names = pd.unique(cells_df.loc[:,"FileName_Original"]) #Extract unique names of images
 
+result_df = pd.DataFrame({'Image number':[], 'Image name':[], 'N Cells':[], 'N Apoptotic':[], 'N Necrotic':[], 'N NA':[]})
+
 for name in image_names:
     cells_df_current = cells_df.loc[cells_df["FileName_Original"] == name] #Extract all rows with same image name
     apoptosis_df_current = apoptosis_df.loc[apoptosis_df["FileName_Original"] == name]
@@ -26,5 +39,21 @@ for name in image_names:
     necrosis_number = necrosis_df_current.shape[0]
     an_df_number = an_df_current.shape[0]
 
-    print(cells_number,apoptosis_number,necrosis_number,an_df_number)
+    #Extract original image number from image name
+    image_number = number_from_imagename(name)
+    print(name)
+
+    #Create df with current data 
+    result_current =  pd.DataFrame({'Image number':[image_number], 'Image name':[name], 'N Cells':[cells_number], 'N Apoptotic':[apoptosis_number], 
+        'N Necrotic':[necrosis_number], 'N NA':[an_df_number]})
+
+    #Append current data to result df
+    result_df = result_df.append(result_current)
+
+result_df = result_df.sort_values(by=['Image name'])
+result_sorted_df = result_df.sort_values(by=['Image number'])
+
+print(result_sorted_df)
+
+
 
