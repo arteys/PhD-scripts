fbd6ed1b8f965d8aaeb26ff1d76c3543f926f2d6
arteys <calamatica@gmail.com>
2023-01-10 01:44:54 +0500

    Add example data for CP_spheroids_processing

--- CP_spheroids_processing.py
+++ CP_spheroids_processing.py
@@ -1,3 +1,15 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+path = "C:/Users/Modern/Desktop/Python/PhD-scripts/Data examples/CP_spheroids_processing/Spheroids.csv"
+
+spheroids_df = pd.read_csv(path)
+
+labels = spheroids_df.columns.values
+
+# print(labels)
+
+sns.scatterplot(data=spheroids_df, x="AreaShape_Area", y="AreaShape_Eccentricity")
+
+plt.show()
