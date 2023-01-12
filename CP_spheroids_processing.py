0b37f04cf52b365f6b6407ff105c240bd7d2381f
arteys <calamatica@gmail.com>
2023-01-12 10:11:15 +0500

    Creating compact df with useful data

--- CP_spheroids_processing.py
+++ CP_spheroids_processing.py
@@ -8,11 +8,16 @@ spheroids_df = pd.read_csv(path)
 
 labels = spheroids_df.columns.values
 
-spheroids_compact_df = spheroids_df[["ObjectNumber","AreaShape_Area","AreaShape_MeanRadius","AreaShape_FormFactor","Intensity_IntegratedIntensity_Grayscale"]]
+spheroids_compact_df = spheroids_df[["ObjectNumber","AreaShape_Area","AreaShape_MeanRadius",
+    "AreaShape_FormFactor","Intensity_IntegratedIntensity_Grayscale"]]
+
+spheroids_compact_df["Type"] = "Agarose_300"
+
+#ADD OBJECT TYPE LABELS FOR DIFFEREND KINDS OF SPHEROIDS
 
 # print(labels)
 
-print(spheroids_compact_df.mean()) #IN PIXELS!!
+print(spheroids_compact_df) #IN PIXELS!!
 
 # sns.scatterplot(data=spheroids_df, x="AreaShape_Area", y="AreaShape_Eccentricity", color=".15")
 # sns.kdeplot(data=spheroids_df, x="AreaShape_Area", y="AreaShape_Eccentricity", levels=5, color="b", linewidths=1)
