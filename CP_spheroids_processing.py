ec47a115216f4bd7cbaac3242096ad274a6cc2ad
arteys <calamatica@gmail.com>
2023-01-14 01:42:23 +0500

    Obvious check_exstance_of_the_folder thing added

--- CP_spheroids_processing.py
+++ CP_spheroids_processing.py
@@ -3,23 +3,33 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 path = "C:/Users/Modern/Desktop/Thesis big files/12-12-22 Spheroids FH/Dish 0.3/Dark/Counted/Spheroids_filtered.csv"
+path_2 = "C:/Users/Modern/Desktop/Thesis big files/29-12-2022 FH on Matrix E 300-20/48H/Counted/Spheroids.csv"
 
 spheroids_df = pd.read_csv(path)
+spheroids_df_2 = pd.read_csv(path_2)
 
-labels = spheroids_df.columns.values
+labels_2 = spheroids_df_2.columns.values
 
 spheroids_compact_df = spheroids_df[["ObjectNumber","AreaShape_Area","AreaShape_MeanRadius",
     "AreaShape_FormFactor","Intensity_IntegratedIntensity_Grayscale"]]
 
+spheroids_compact_df_2 = spheroids_df_2[["ObjectNumber","AreaShape_Area","AreaShape_MeanRadius",
+    "AreaShape_FormFactor","Intensity_IntegratedIntensity_Grayscale"]]
+
 spheroids_compact_df["Type"] = "Agarose_300"
+spheroids_compact_df_2["Type"] = "EDC_300-20"
 
-#ADD OBJECT TYPE LABELS FOR DIFFEREND KINDS OF SPHEROIDS
 
-# print(labels)
+# print(spheroids_compact_df_2)
 
-print(spheroids_compact_df) #IN PIXELS!!
+frames_df = [spheroids_compact_df,spheroids_compact_df_2]
+ultimate_df = pd.concat(frames_df)
 
-# sns.scatterplot(data=spheroids_df, x="AreaShape_Area", y="AreaShape_Eccentricity", color=".15")
-# sns.kdeplot(data=spheroids_df, x="AreaShape_Area", y="AreaShape_Eccentricity", levels=5, color="b", linewidths=1)
+print(ultimate_df)
 
-# plt.show()
+# print(spheroids_compact_df) #IN PIXELS!!
+
+sns.scatterplot(data=ultimate_df, x="AreaShape_MeanRadius", y="AreaShape_FormFactor", hue = "Type")
+sns.kdeplot(data=ultimate_df, x="AreaShape_MeanRadius", y="AreaShape_FormFactor", hue = "Type", levels=5, color="b", linewidths=1)
+
+plt.show()
