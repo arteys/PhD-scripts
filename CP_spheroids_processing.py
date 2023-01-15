c97d2d4303cc4ea746d466b3778e0296159c3405
arteys <calamatica@gmail.com>
2023-01-16 00:06:39 +0500

    Some fuss with CP projects for spheroids. Also px to mkm conversion in python script for spheroid processing

--- CP_spheroids_processing.py
+++ CP_spheroids_processing.py
@@ -2,34 +2,57 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-path = "C:/Users/Modern/Desktop/Thesis big files/12-12-22 Spheroids FH/Dish 0.3/Dark/Counted/Spheroids_filtered.csv"
+path = "C:/Users/Modern/Desktop/Thesis big files/26-12-2022 FH on Matrix S/Counted/Shperoids_filtered.csv"
 path_2 = "C:/Users/Modern/Desktop/Thesis big files/29-12-2022 FH on Matrix E 300-20/48H/Counted/Spheroids.csv"
+path_3 = "C:/Users/Modern/Desktop/Thesis big files/12-12-22 Spheroids FH/Dish 0.4/Counted/Shperoids_filtered.csv"
 
 spheroids_df = pd.read_csv(path)
 spheroids_df_2 = pd.read_csv(path_2)
+spheroids_df_3 = pd.read_csv(path_3)
 
 labels_2 = spheroids_df_2.columns.values
+print(labels_2)
 
-spheroids_compact_df = spheroids_df[["ObjectNumber","AreaShape_Area","AreaShape_MeanRadius",
-    "AreaShape_FormFactor","Intensity_IntegratedIntensity_Grayscale"]]
+spheroids_df["AreaShape_EquivalentDiameter"] = spheroids_df["AreaShape_EquivalentDiameter"]/1.49
+spheroids_df_2["AreaShape_EquivalentDiameter"] = spheroids_df["AreaShape_EquivalentDiameter"]/1.49
+spheroids_df_3["AreaShape_EquivalentDiameter"] = spheroids_df["AreaShape_EquivalentDiameter"]/3.02
 
-spheroids_compact_df_2 = spheroids_df_2[["ObjectNumber","AreaShape_Area","AreaShape_MeanRadius",
-    "AreaShape_FormFactor","Intensity_IntegratedIntensity_Grayscale"]]
+spheroids_compact_df = spheroids_df[["ObjectNumber","AreaShape_Area","AreaShape_EquivalentDiameter",
+    "AreaShape_FormFactor","AreaShape_MeanRadius","Intensity_IntegratedIntensity_Grayscale"]]
 
-spheroids_compact_df["Type"] = "Agarose_300"
-spheroids_compact_df_2["Type"] = "EDC_300-20"
+spheroids_compact_df_2 = spheroids_df_2[["ObjectNumber","AreaShape_Area","AreaShape_EquivalentDiameter",
+    "AreaShape_FormFactor","AreaShape_MeanRadius","Intensity_IntegratedIntensity_Grayscale"]]
+
+spheroids_compact_df_3 = spheroids_df_3[["ObjectNumber","AreaShape_Area","AreaShape_EquivalentDiameter",
+    "AreaShape_FormFactor","AreaShape_MeanRadius","Intensity_IntegratedIntensity_Grayscale"]]
+
+
+spheroids_compact_df["Type"] = "EDC 600-20"
+spheroids_compact_df_3["Type"] = "Agarose"
+spheroids_compact_df_2["Type"] = "EDC 300-20"
 
 
 # print(spheroids_compact_df_2)
 
-frames_df = [spheroids_compact_df,spheroids_compact_df_2]
+frames_df = [spheroids_compact_df,spheroids_compact_df_2,spheroids_compact_df_3]
 ultimate_df = pd.concat(frames_df)
 
-print(ultimate_df)
+# print(ultimate_df)
 
-# print(spheroids_compact_df) #IN PIXELS!!
 
-sns.scatterplot(data=ultimate_df, x="AreaShape_MeanRadius", y="AreaShape_FormFactor", hue = "Type")
-sns.kdeplot(data=ultimate_df, x="AreaShape_MeanRadius", y="AreaShape_FormFactor", hue = "Type", levels=5, color="b", linewidths=1)
+# fig, ax =plt.subplots(1,2)
+ax1 = sns.scatterplot(data=ultimate_df, x="AreaShape_EquivalentDiameter", y="AreaShape_FormFactor", hue = "Type")
+# ax2 = sns.scatterplot(data=ultimate_df, x="AreaShape_EquivalentDiameter", y="Intensity_IntegratedIntensity_Grayscale", hue = "Type",ax=ax[1])
+# ax = sns.kdeplot(data=ultimate_df, x="AreaShape_EquivalentDiameter", y="Intensity_IntegratedIntensity_Grayscale", hue = "Type", levels=5, linewidths=1)
+# sns.swarmplot(data=ultimate_df, y="AreaShape_EquivalentDiameter", x="AreaShape_FormFactor", hue = "Type")
+# sns.relplot(data=ultimate_df, y="AreaShape_EquivalentDiameter", x="AreaShape_FormFactor", hue = "Type")
+
+
+
+ax1.set(xlabel='Equvalent diameter, μm', ylabel='Form factor')
+ax1.set_aspect('auto')
+
+# ax2.set(xlabel='Equvalent diameter, μm', ylabel='Integral intensity, A.U.')
+
 
 plt.show()
--- Rename_file_in_folder.py
+++ Rename_file_in_folder.py
@@ -2,7 +2,7 @@ from os import walk
 import os
 
 files =[]
-dir_path = "C:/Users/Modern/Desktop/Thesis big files/12-12-22 Spheroids FH/Dish 0.3/Segmentation"
+dir_path = "C:/Users/Modern/Desktop/Thesis big files/12-12-22 Spheroids FH/Dish 0.4/"
 # dir_path = "C:/Users/Modern/Desktop/Thesis big files/12-12-22 Spheroids FH/Dish 0.3/"
 
 i = 0
@@ -11,7 +11,7 @@ for path in os.listdir(dir_path):
     # check if current path is a file
     if os.path.isfile(os.path.join(dir_path, path)):
         old_name = os.path.join(dir_path, path)
-        new_name = dir_path + "/" +str(f'{i:03}') + '_Probabilities' + '.png'
+        new_name = dir_path + "/" +str(f'{i:03}') + '_Original' + '.png'
         print(old_name)
         print(new_name)
         os.rename(old_name,new_name)
